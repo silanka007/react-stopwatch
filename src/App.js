70bfa3441b0276a609017c0a22fa2deb79640fb9
Paul Onyekwelu <onyekwelupaulo@gmail.com>
2021-11-17 12:22:56 +0100

    implemented stopwatch using reactjs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [timer, setTimer] = useState(0);
+  const [isTiming, setIsTiming] = useState(0);
+
+  useEffect(() => {
+    let interval;
+    if (isTiming) {
+      interval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isTiming]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <div>
+          <span>{`0${Math.floor((timer / 60000) % 60)}`.slice(-2)}</span>m:
+          <span>{`0${Math.floor((timer / 1000) % 60)}`.slice(-2)}</span>s:
+          <span>{`0${Math.floor((timer / 10) % 100)}`.slice(-2)}</span>
+        </div>
+        <div>
+          <button onClick={() => setIsTiming(true)}>Start</button>
+          <button onClick={() => setIsTiming(false)}>Stop</button>
+          <button onClick={() => setTimer(0)}>Reset</button>
+        </div>
+      </div>
     </div>
   );
 }
